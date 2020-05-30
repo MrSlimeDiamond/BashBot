1cb9c4c8b4e6e084d23982c075e65695801c4158
MrSlimeDiamond <findlayrichardson3@gmail.com>
2020-05-30 06:35:10 +0200

    Update README, config, and add sleep to bashbot.sh build (:

--- bashbot.sh
+++ bashbot.sh
@@ -61,8 +61,10 @@ if [ "$1" == "confirmbuild" ]; then
 	sleep 1
 	screen -S $irssiScreenName -X stuff "/connect $server\n"
         echo -ne "***---\r"
+	slerp 10
         screen -S $irssiScreenName -X stuff "/join $channel\n"
         echo -ne "****--\r"
+	sleep 1
 	screen -S $irssiScreenName -X stuff "/log start $logfile\n"
 	echo -ne "*****-\r"
 	sleep 1
--- config.sh
+++ config.sh
@@ -4,5 +4,5 @@ irssiScreenName='irssi' # Name of irssi screen
 channel="#yourchannel" # IRC Channel
 server="irc.example.com" # IRC server
 lckfile="/home/bashbot/bashbot.lck" # Lock file
-logfile="/home/bashbot/$server/$channel" # Where irssi logs
+logfile="/home/bashbot/irclogs/$server/$channel" # Where irssi logs
 makelckfile="true" # Prevent BashBot from being run more than once
