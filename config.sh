90e75e7442b7ac45392f52b40f1e517e45fa194d
MrSlimeDiamond <findlayrichardson3@gmail.com>
2020-05-30 07:24:59 +0200

    remove logfile from quotes

--- config.sh
+++ config.sh
@@ -4,6 +4,5 @@ irssiScreenName='irssi' # Name of irssi screen
 channel="#yourchannel" # IRC Channel
 server="irc.example.com" # IRC server
 lckfile="/home/bashbot/bashbot.lck" # Lock file
-logfile="~/irclogs/$server/$channel" # Where irssi logs are, it's recommended you don't change this setting (:
-logfile_send="~/irclogs/$server/\\$channel" # Channel with a backslash infront of it
+logfile=~/irclogs/$server/$channel # Where irssi logs are, it's recommended you don't change this setting (:
 makelckfile="true" # Prevent BashBot from being run more than once
