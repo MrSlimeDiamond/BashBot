2f0469ec36e7ef57322e91b2aef822d011f1b1ca
MrSlimeDiamond <findlayrichardson3@gmail.com>
2020-05-30 20:58:08 +0200

    Allow changing of nickname, realname, username, and (hopefully) allow users to run it in their own directory if desired

--- bashbot.sh
+++ bashbot.sh
@@ -3,15 +3,48 @@
 
 # Config is in ./config.sh :) 
 
-version="1.19 beta" # Don't touch me
+version="1.20 beta" # Don't touch me
 run=true # Don't change: for while loop :)
 time=$(date +%H:%M:%S) # Format for time
 # Clients
-sendtoirc="/home/bashbot/sendtoirc.sh"
+#sendtoirc="/home/bashbot/sendtoirc.sh"
+sendtoirc="./sendtoirc.sh"
 #config="/home/bashbot/config.sh"
 #. /home/bashbot/config.sh
 #. ./config.sh
-source config.sh
+source ./config.sh
+
+# Get the directory the file is in, this script is stolen from stackoverflow (:
+# Currently useless
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do 
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $TARGET == /* ]]; then
+    echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+    SOURCE="$TARGET"
+  else
+    DIR="$( dirname "$SOURCE" )"
+    echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+    SOURCE="$DIR/$TARGET" 
+  fi
+done
+echo "SOURCE is '$SOURCE'"
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+if [ "$DIR" != "$RDIR" ]; then
+  echo "DIR '$RDIR' resolves to '$DIR'"
+fi
+echo "DIR is '$DIR'"
+
+logerror(){
+echo -e "\e[31m$time [ERROR] $1\e[00m"
+}
+
+logwarn(){
+echo -e "\e[33m$time [WARN] $1\e[00m"
+}
+
 log(){
 echo "$time [LOG] $1"
 }
@@ -54,36 +87,45 @@ Note: As of version 1.19 beta, doing start will automatically build
 Requirements:
 Irssi
 Screen
-It is recommended you put this on a seperate account named bashbot, because it creates files.
+It is recommended you put this on a seperate account named bashbot.
 Type ./bashbot.sh confirmbuild to continue."
 exit
 fi
 #if [ "$1" == "confirmbuild" ]; then
 	buildbot(){
 	screen -dmS $irssiScreenName
-	echo -ne "*-----\r"
+	#echo -ne "*-----\r"
+	echo -ne "Starting irssi screen... name: $irssiScreenName\r"
 	sleep 1
 	screen -S $irssiScreenName -X stuff "irssi\n"
-	echo -ne "**----\r"
+	echo -ne "Starting irssi...\r"
+	sleep 1
+	echo -ne "Setting up user...\r"
+	screen -S $irssiScreenName -X stuff "/set nick $nick\n"
+	#echo -ne "Setting up user..."
+	screen -S $irssiScreenName -X stuff "/set real_name $realname\n"
+	sleep 1
+	screen -S $irssiScreenName -X stuff "/set user_name $username\n"
 	sleep 1
 	screen -S $irssiScreenName -X stuff "/connect $server\n"
-        echo -ne "***---\r"
+	echo -ne "Connecting to $server\r"
 	sleep 10
         screen -S $irssiScreenName -X stuff "/join $channel\n"
-        echo -ne "****--\r"
+        echo -ne "joining $channel\r"
 	sleep 1
 	#screen -S $irssiScreenName -X stuff "/log start $logfile\n"
 	screen -S $irssiScreenName -X stuff "/log open -targets $channel $logfile\n"
-	echo -ne "*****-\r"
+	echo -ne "Setting up logging (part 1)\r"
 	sleep 1
 	#screen -S $irssiScreenName -X stuff "/log open -targets $channel $logfile\n"
 	screen -S $irssiScreenName -X stuff "/log start $logfile\n"
+	echo -ne "Setting up logging (part 2)\r"
 	# Silly me did irssi commands the wrong way around :I
 	#echo -ne "******\r"
 	sleep 1
 	#screen -S $irssiScreenName -X stuff "/connect $server\n"
-	echo -ne "******\r"
-	echo -ne "Build complete\r"
+	#echo -ne "******\r"
+	echo -ne "Finished\r"
 	#sleep 1
 	#screen -S $irssiScreenName -X stuff "/join $channel\n"
 	#echo -ne "******\r"
@@ -106,7 +148,8 @@ echo -e "\e[32m$time [BashBot] Starting build process...\e[00m"
 buildbot
 echo "Note: Using build connects the bot to irc, start will make it respond to commands. Use the stop command to take the bot offline"
 if [ -f "$lckfile" ]; then
-	echo -e "\e[31m$time [ERROR] lckfile $lckfile exists, exiting...\e[00m"
+#	echo -e "\e[31m$time [ERROR] lckfile $lckfile exists, exiting...\e[00m"
+	logerror "lckfile $lckfile exists, exiting..."
 	echo -e "This means BashBot may already be running."
 	exit
 fi
@@ -117,7 +160,8 @@ if [ "$makelckfile" = "true" ]; then
 	log "Creating lckfile: $lckfile"
 	touch $lckfile
 	log "Created lckfile sucsesfully"
-	log "Please note: Do not use kill -9 on this unless it is completely nececary. If you experience errors when starting, try rm -f $lckfile"
+	#log "Please note: Do not use kill -9 on this unless it is completely nececary. If you experience errors when starting, try rm -f $lckfile"
+	logwarn "Using kill -9 may cause errors when starting. If you have trouble starting, try removing $lckfile"
 fi
 
 #echo "Creating lckfile: $lckfile"
--- config.sh
+++ config.sh
@@ -3,7 +3,10 @@
 irssiScreenName='irssi' # Name of irssi screen
 channel="#yourchannel" # IRC Channel
 server="irc.example.com" # IRC server
-lckfile="/home/bashbot/bashbot.lck" # Lock file
-logfile=~/irclogs/$server/$channel # Where irssi logs are, it's recommended you don't change this setting (:
-quitmessage="Shutting down"
-makelckfile="true" # Prevent BashBot from being run more than once
+lckfile="./bashbot.lck" # Lock file
+logfile=./irclogs/$server/$channel # Where irssi logs are, it's recommended you don't change this setting (:
+nickname="bashbot" # Nickname of the IRC bot
+realname="Bash Bot" # Real name of the IRC bot
+username="bashbot" # Username of the IRC bot
+quitmessage="Shutting down" # Shutdown message
+makelckfile="true" # Prevent BashBot from being run more than once in the same folder
